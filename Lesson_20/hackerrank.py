7c3a5f371970bba9781916af0905aa299445117e
Konstantin <39737614+healfy@users.noreply.github.com>
2018-08-23 17:37:13 +0300

    Update hackerrank.py

--- Lesson_20/hackerrank.py
+++ Lesson_20/hackerrank.py
@@ -8,3 +8,19 @@ def insertNodeAtPosition(head, data, position):
     node.next = current_node.next
     current_node.next = node
     return head
+
+
+def makeAnagram(a, b):    
+    p = Counter(a)
+    z = Counter(b)
+    d = p - z
+    c = z - p
+    e = d + c
+    return len(list(e.elements()))
+
+def alternatingCharacters(s):
+    d = []
+    for i in range(len(s) - 1):
+        if s[i+1] == s[i]:
+            d.append(s[i+1])
+    return len(d)
